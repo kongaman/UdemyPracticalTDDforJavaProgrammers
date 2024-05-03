d86d435d3ff2b397b869ab7c516ad85a3e9d9265
kongaman <christian.kranz@gmail.com>
2024-05-03 19:05:08 +0200

    Chapter 3
    Test 3 pass

--- src/main/java/ex1/isbnValidator/ValidateISBN.java
+++ src/main/java/ex1/isbnValidator/ValidateISBN.java
@@ -3,10 +3,17 @@ package ex1.isbnValidator;
 public class ValidateISBN {
 
 	public boolean checkISBN(String isbn) {
-		if (isbn == "0140449116") {
-			return true;
+		int total = 0;
+
+		for (int i = 0; i < 10; i++) {
+			total += isbn.charAt(i) * (10 - i);
 		}
-		return false;
+
+		if (total % 11 == 0) {
+			return true;
+		} else {
+			return false;
+		}
 	}
 
 }
