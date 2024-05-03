4d7983b27656840f0491706ec2a2d7c64c51c3e0
kongaman <christian.kranz@gmail.com>
2024-05-03 19:41:18 +0200

    Chapter 4
    nonNumericISBNsAreNotAllowed - pass
    refactor - nothing to do

--- src/main/java/ex1/isbnValidator/ValidateISBN.java
+++ src/main/java/ex1/isbnValidator/ValidateISBN.java
@@ -9,7 +9,11 @@ public class ValidateISBN {
 		int total = 0;
 
 		for (int i = 0; i < 10; i++) {
+			if (!Character.isDigit(isbn.charAt(i))) {
+				throw new NumberFormatException("ISBN numbers can only contain digits");
+			}
 			total += isbn.charAt(i) * (10 - i);
+
 		}
 
 		if (total % 11 == 0) {
