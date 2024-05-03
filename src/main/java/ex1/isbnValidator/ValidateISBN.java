29b1f25f6726480cfc1fe7d481dd8732a7611d00
kongaman <christian.kranz@gmail.com>
2024-05-03 17:39:47 +0200

    Basic Setup - Chapter 2

---
+++ src/main/java/ex1/isbnValidator/ValidateISBN.java
@@ -0,0 +1,5 @@
+package ex1.isbnValidator;
+
+public class ValidateISBN {
+
+}
---
+++ src/test/java/ex1/isbnValidator/ValidateISBNTest.java
@@ -0,0 +1,14 @@
+package ex1.isbnValidator;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ValidateISBNTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
