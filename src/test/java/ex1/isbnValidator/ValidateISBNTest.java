1bf4b62a6bdb914523889d97ef455f0f0bc2fe37
kongaman <christian.kranz@gmail.com>
2024-05-03 18:38:39 +0200

    Chapter 3
    Test 2 - fail

--- src/test/java/ex1/isbnValidator/ValidateISBNTest.java
+++ src/test/java/ex1/isbnValidator/ValidateISBNTest.java
@@ -1,5 +1,6 @@
 package ex1.isbnValidator;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
@@ -11,6 +12,13 @@ class ValidateISBNTest {
 		ValidateISBN validator = new ValidateISBN();
 		boolean result = validator.checkISBN(140449116);
 		assertTrue(result);
+	}
+
+	@Test
+	void checkInvalidISBN() {
+		ValidateISBN validator = new ValidateISBN();
+		boolean result = validator.checkISBN(140449117);
+		assertFalse(result);
 	}
 
 }
