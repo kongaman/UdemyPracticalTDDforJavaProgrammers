bf935df27ae821f911900ab4d106f6821e3182d5
kongaman <christian.kranz@gmail.com>
2024-05-17 16:00:40 +0200

    Chapter 7 - Stubs
    Creation of a test stub that functions as the external webservice and
    can be injected into the class that we want to test.

--- src/test/java/ex1/isbnValidator/StockManagementTests.java
+++ src/test/java/ex1/isbnValidator/StockManagementTests.java
@@ -8,8 +8,20 @@ class StockManagementTests {
 
 	@Test
 	void testCanGetACorrectLocatorCode() {
-		String isbn = "0140177396";
+
+		// This simulates the external webservice that is called in production code
+		ExternalISBNDataService testService = new ExternalISBNDataService() {
+
+			@Override
+			public Book lookup(String isbn) {
+				return new Book(isbn, "Of Mice And Men", "J. Steinbeck");
+			}
+		};
+
 		StockManager stockmanager = new StockManager();
+		stockmanager.setService(testService);
+
+		String isbn = "0140177396";
 		String locatorCode = stockmanager.getLocatorCode(isbn);
 		assertEquals("7396J4", locatorCode);
 	}
