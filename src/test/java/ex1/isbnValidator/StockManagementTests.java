e8bb5b2ce4eb448f8820f6e6539e3af7f3a0c101
kongaman <christian.kranz@gmail.com>
2024-05-18 13:16:44 +0200

    Chapter 9 - Fakes and Tauntologies
    Test "testCanGetACorrectLocatorCode" rewritten with mockito

--- src/test/java/ex1/isbnValidator/StockManagementTests.java
+++ src/test/java/ex1/isbnValidator/StockManagementTests.java
@@ -11,32 +11,50 @@ import org.junit.jupiter.api.Test;
 
 class StockManagementTests {
 
+//	@Test
+//	void testCanGetACorrectLocatorCode() {
+//
+//		// This simulates the external webservice that is called in production code
+//		ExternalISBNDataService testWebService = new ExternalISBNDataService() {
+//
+//			@Override
+//			public Book lookup(String isbn) {
+//				return new Book(isbn, "Of Mice And Men", "J. Steinbeck");
+//			}
+//		};
+//
+//		// This simulates the external database that is called in production code
+//		ExternalISBNDataService testDatabaseService = new ExternalISBNDataService() {
+//
+//			@Override
+//			public Book lookup(String isbn) {
+//				return null;
+//			}
+//		};
+//
+//		StockManager stockmanager = new StockManager();
+//		// this sets our testWebService as the service to use for tests instead of the production webservice
+//		stockmanager.setWebService(testWebService);
+//		// this sets our testDatabaseService as the service to use for tests instead of the production database
+//		stockmanager.setDatabaseService(testDatabaseService);
+//
+//		String isbn = "0140177396";
+//		String locatorCode = stockmanager.getLocatorCode(isbn);
+//		assertEquals("7396J4", locatorCode);
+//	}
+
 	@Test
 	void testCanGetACorrectLocatorCode() {
 
-		// This simulates the external webservice that is called in production code
-		ExternalISBNDataService testWebService = new ExternalISBNDataService() {
+		ExternalISBNDataService webService = mock(ExternalISBNDataService.class);
+		ExternalISBNDataService databaseService = mock(ExternalISBNDataService.class);
 
-			@Override
-			public Book lookup(String isbn) {
-				return new Book(isbn, "Of Mice And Men", "J. Steinbeck");
-			}
-		};
-
-		// This simulates the external database that is called in production code
-		ExternalISBNDataService testDatabaseService = new ExternalISBNDataService() {
-
-			@Override
-			public Book lookup(String isbn) {
-				return null;
-			}
-		};
+		when(databaseService.lookup("0140177396")).thenReturn(null);
+		when(webService.lookup("0140177396")).thenReturn(new Book("0140177396", "Of Mice And Men", "J. Steinbeck"));
 
 		StockManager stockmanager = new StockManager();
-		// this sets our testWebService as the service to use for tests instead of the production webservice
-		stockmanager.setWebService(testWebService);
-		// this sets our testDatabaseService as the service to use for tests instead of the production database
-		stockmanager.setDatabaseService(testDatabaseService);
+		stockmanager.setWebService(webService);
+		stockmanager.setDatabaseService(databaseService);
 
 		String isbn = "0140177396";
 		String locatorCode = stockmanager.getLocatorCode(isbn);
@@ -77,8 +95,8 @@ class StockManagementTests {
 		ExternalISBNDataService databaseService = mock(ExternalISBNDataService.class);
 		ExternalISBNDataService webService = mock(ExternalISBNDataService.class);
 
-		when(databaseService.lookup("0140177396")).thenReturn(null);
-		when(webService.lookup("0140177396")).thenReturn(new Book("0140177396", "abc", "abc"));
+		when(databaseService.lookup(anyString())).thenReturn(null);
+		when(webService.lookup(anyString())).thenReturn(new Book("0140177396", "abc", "abc"));
 
 		StockManager stockmanager = new StockManager();
 		stockmanager.setWebService(webService);
